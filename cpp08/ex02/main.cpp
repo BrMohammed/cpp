6e4515fbaf1a633f89652b2712a7b875ebcc6e36
mohammed <togachivon@gmail.com>
2022-10-21 20:02:52 +0100

    END_CPP

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,4 +1,5 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
@@ -26,3 +27,29 @@ int main()
     std::cout << " "<< s.size() << std::endl;
     return 0;
 }
+
+// int main()
+// {
+//     std::list<int> mstack;
+//     mstack.push_back(5);
+//     mstack.push_back(17);
+//     std::cout << mstack.back() << std::endl;
+//     std::cout << mstack.size() << std::endl;
+//     mstack.push_back(3);
+//     mstack.push_back(5);
+//     mstack.push_back(737);
+//     mstack.push_back(0);
+//     std::list<int>::iterator it = mstack.begin();
+//     std::list<int>::iterator ite = mstack.end();
+//     ++it;
+//     --it;
+//     while (it != ite)
+//     {
+//     std::cout << *it << std::endl;
+//     ++it;
+//     }
+//     // std::stack<int> s(mstack);
+//     // std::cout << " "  << mstack.size() << std::endl;
+//     // std::cout << " "<< s.size() << std::endl;
+//     return 0;
+// }
